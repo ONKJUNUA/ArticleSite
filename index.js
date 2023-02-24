c917906405f5016b28ee669b9e1b144b76d97113
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-02-24 15:02:56 +0100

    site finished (dark mode added)

---
+++ index.js
@@ -0,0 +1,69 @@
+let activeIndex = 0;
+
+const slides = document.getElementsByTagName("article");
+
+const handleLeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+  currentSlide.dataset.status = "after";
+  
+  nextSlide.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const handleRightClick = () => {
+  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentSlide.dataset.status = "before";
+  
+  nextSlide.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const nav = document.querySelector("nav");
+
+const handleNavToggle = () => {  
+  nav.dataset.transitionable = "true";
+  
+  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
+}
+
+window.matchMedia("(max-width: 800px)").onchange = e => {
+  nav.dataset.transitionable = "false";
+
+  nav.dataset.toggled = "false";
+};
+
+var counter=0;
+
+const root = document.querySelector(':root');
+
+const LightMode = () => {
+    
+    if(counter===0){
+        counter++;
+        root.style.setProperty('--color-color', '#111');
+        root.style.setProperty('--border-color', '#666');
+        root.style.setProperty('--background-color', '#888');
+    }
+    else {
+        counter--;
+        root.style.setProperty('--color-color', '#fff');
+        root.style.setProperty('--border-color', '#222');
+        root.style.setProperty('--background-color', '#111');
+    }
+}
